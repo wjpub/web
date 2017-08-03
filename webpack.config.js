2d247f4cfbe2fb2e00d3f351d551dabbf91e54d9
WangJian <wangjian@deepwise.com>
2017-08-03 15:05:50 +0800

    Asset Management

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,40 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/i,
+                use: [
+                    'file-loader',
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                use: [
+                    'file-loader',
+                ]
+            },
+            {
+                test: /\.(csv|tsv)$/i,
+                use: [
+                    'csv-loader',
+                ]
+            },
+            {
+                test: /\.xml$/i,
+                use: [
+                    'xml-loader'
+                ]
+            }
+        ]
     }
 }
