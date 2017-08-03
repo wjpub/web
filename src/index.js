3de1c80a3c363d11c76348751330aa9f37175c66
WangJian <wangjian@deepwise.com>
2017-08-03 16:18:15 +0800

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
 function component() {
 
     var element = document.createElement('div');
+    var btn = document.createElement('button');
     element.innerHTML = _.join(['hello', 'webpack'], '  ');
+
+    btn.innerHTML = 'click me and check the console!';
+    btn.onclick = function(){printMe()};
+    element.appendChild(btn);
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,68 @@
 const path = require("path");
+const Webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js',
+        vender: [
+            'lodash',
+        ]
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].[chunkhash].js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif|svg)$/i,
-                use: [
-                    'file-loader',
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                use: [
-                    'file-loader',
-                ]
-            },
-            {
-                test: /\.(csv|tsv)$/i,
-                use: [
-                    'csv-loader',
-                ]
-            },
-            {
-                test: /\.xml$/i,
-                use: [
-                    'xml-loader'
-                ]
-            }
+            // {
+            //     test: /\.css$/,
+            //     use: [
+            //         'style-loader',
+            //         'css-loader'
+            //     ]
+            // },
+            // {
+            //     test: /\.(png|jpg|jpeg|gif|svg)$/i,
+            //     use: [
+            //         'file-loader',
+            //     ]
+            // },
+            // {
+            //     test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            //     use: [
+            //         'file-loader',
+            //     ]
+            // },
+            // {
+            //     test: /\.(csv|tsv)$/i,
+            //     use: [
+            //         'csv-loader',
+            //     ]
+            // },
+            // {
+            //     test: /\.xml$/i,
+            //     use: [
+            //         'xml-loader'
+            //     ]
+            // }
         ]
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'OUTPUT_MANAGEMENT',
+        }),
+        new ManifestPlugin({
+        }),
+        new Webpack.optimize.CommonsChunkPlugin({
+            name: 'vender'
+        }),
+        new Webpack.optimize.CommonsChunkPlugin({
+            name: 'runtime'
+        }),
+    ]
 }
